87e700df8bb6bb453eb26d57aef6091f97844fa5
Arthur Morais <mlkliso4000@gmail.com>
2025-04-25 10:23:56 -0300

    ex 6

--- feitos sozinhos/ex06.js
+++ feitos sozinhos/ex06.js
@@ -1,6 +1,6 @@
 let pessoasDaMesa = [ 'Gustavo', 'Murilo', 'Daniel', 'Gabriela Yamamoto', 'Beatriz', 'Bianca'];
 let quantidade = pessoasDaMesa.length;
 
-for(let i = quantidade - 1; i > -1; i--) {
+for(let i = quantidade - 1; i >= 0; i--) {
     console.log(pessoasDaMesa[i]);
 }
