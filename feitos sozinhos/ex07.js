14f48541dde2439102b6cb4dafa939a219dc7acd
Arthur Morais <mlkliso4000@gmail.com>
2025-04-25 10:22:54 -0300

    ex 7

--- feitos sozinhos/ex07.js
+++ feitos sozinhos/ex07.js
@@ -1,1 +1,7 @@
-let notas = [ '10', '10', '10', '10'];
+let notas = [8.5, 7.0, 10, 10];
+let i = 0;
+
+while (i < notas.length) {
+console.log(notas[i]);
+i++;
+}
---
+++ feitos sozinhos/ex08.js
@@ -0,0 +1,7 @@
+let notas = [ '10', '10', '10', '10'];
+let quantidadeNotas = notas.length;
+
+while (notas <= quantidadeNotas){
+    console.log(notas);
+    quantidadeNotas;
+}
